cfdfa647b55b5b031fcfbe07ec0e5c0ed0bda693
Rodger6666 <3410684778@qq.com>
2025-12-12 11:07:20 +0800

    实验步骤3-实现界面转换

---
+++ doctorview.cpp
@@ -0,0 +1,14 @@
+#include "doctorview.h"
+#include "ui_doctorview.h"
+
+DocterView::DocterView(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::DocterView)
+{
+    ui->setupUi(this);
+}
+
+DocterView::~DocterView()
+{
+    delete ui;
+}
---
+++ masterview.h
@@ -0,0 +1,48 @@
+#ifndef MASTERVIEW_H
+#define MASTERVIEW_H
+
+#include <QWidget>
+#include "loginview.h"
+#include "doctorview.h"
+#include "departmentview.h"
+#include "patienteditview.h"
+#include "patientview.h"
+#include "welcomeview.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui {
+class MasterView;
+}
+QT_END_NAMESPACE
+
+class MasterView : public QWidget
+{
+    Q_OBJECT
+
+public:
+    MasterView(QWidget *parent = nullptr);
+    ~MasterView();
+
+public slots:
+    void goLoginView();
+    void goWelcomView();
+    void goDoctorView();
+    void goDepartmentView();
+    void goPatientEditView();
+    void goPatientView();
+    void goPreviousView();
+
+private:
+    void pushWidgetToStackView(QWidget *widget);
+
+    Ui::MasterView *ui;
+
+    WelcomeView *welcomeView;
+    DocterView *doctorView;
+    PatientView *patientView;
+    DepartmentView *departmentView;
+    LoginView *loginView;
+    PatientEditView *patientEditView;
+
+};
+#endif // MASTERVIEW_H
